b21db65bc8f4fb38535665973ebf747ac2b7f992
hailey ham <roserealslow@gmail.com>
2023-10-05 14:45:07 +0900

    Socket.io를 사용하여 서버-클라이언트 연결 설정 완료

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ const io = new Server(server, {
     },
 });
 
+io.on("connection", (socket) => { //connection 이벤트핸들러, client가 server 연결될 때마다 실행 / socket 함수 내부에서 연결된 각 client 작업 수행, socket 객체는 연결된 개별 client이고 client와 server 통신 담당
+    console.log(`User Connected : ${socket.id}`) // clent server 연결되면 고유 socket ID 출력
+})
+
 server.listen(3001, () => {
     console.log("SERVER IS RUNNING")
 })
