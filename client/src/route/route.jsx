1ff63e15d17b860f5ffe205c2edc8d946f0c7534
hailey ham <roserealslow@gmail.com>
2023-10-06 04:51:54 +0900

    chatRoom 페이지 경로변경

--- client/src/route/route.jsx
+++ client/src/route/route.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from '../pages/Home/Main';
 import Chat from '../pages/chat/Chat';
+import ChatRoom from '../pages/chat/ChatRoom';
 
 export default function route() {
     return (
@@ -9,6 +10,7 @@ export default function route() {
             <Routes>
                 <Route path="/" element={<Main />} />
                 <Route path="/chat" element={<Chat />} />
+                <Route path="/chattingRoom" element={<ChatRoom />} />
             </Routes>
         </BrowserRouter>
     )
