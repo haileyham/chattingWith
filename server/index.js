e51ea88310f12c4ea9885602c52531ac826f9c34
hailey ham <roserealslow@gmail.com>
2024-04-30 18:36:27 +0900

    client) cors origin 주소변경

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: "https://visible-belita-haileyham.koyeb.app/",
+    origin: "https://chatting-with.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
