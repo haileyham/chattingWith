905fd23689fcf3049ac2cf8dab171cc22d8629f7
hailey ham <roserealslow@gmail.com>
2024-04-30 15:37:24 +0900

    chatting server 배포주소 업데이트

--- server/index.js
+++ server/index.js
@@ -9,38 +9,38 @@ app.use(cors());
 const server = http.createServer(app)
 
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: "https://visible-belita-haileyham.koyeb.app/",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => { //connection 이벤트핸들러, client가 server 연결될 때마다 실행 / socket 함수 내부에서 연결된 각 client 작업 수행, socket 객체는 연결된 개별 client이고 client와 server 통신 담당
-    console.log(`User Connected : ${socket.id}`) // clent server 연결되면 고유 socket ID 출력
+  console.log(`User Connected : ${socket.id}`) // clent server 연결되면 고유 socket ID 출력
 
-    // 모두에게 보내기
-    socket.on("send_message", (data) => { // client에서 보낸 data
-        // console.log(data);
-        socket.broadcast.emit("receive_message", data); // server에서 client로 메시지 전송 / 메시지 보낸 client에는 안 보내고, 다른 모든 client는 수신
-    })
+  // 모두에게 보내기
+  socket.on("send_message", (data) => { // client에서 보낸 data
+    // console.log(data);
+    socket.broadcast.emit("receive_message", data); // server에서 client로 메시지 전송 / 메시지 보낸 client에는 안 보내고, 다른 모든 client는 수신
+  })
 
-    // 방에 입장하기
-    socket.on("join_room", (data) => {
-        socket.join(data)
-    })
+  // 방에 입장하기
+  socket.on("join_room", (data) => {
+    socket.join(data)
+  })
 
-    // 방에만 보내기
-    socket.on("send_message_Room", (data) => {
-        socket.to(data.room).emit("receive_message", data)
-    })
+  // 방에만 보내기
+  socket.on("send_message_Room", (data) => {
+    socket.to(data.room).emit("receive_message", data)
+  })
 
-    // 방입장
-    socket.on("user_join", ({ room, user }) => {
-        const joinMessage = `${user}님이 채팅방 ${room}에 입장했습니다.`
-        socket.to(room).emit("receive_message", { message: joinMessage, user: "관리자" })
-    })
+  // 방입장
+  socket.on("user_join", ({ room, user }) => {
+    const joinMessage = `${user}님이 채팅방 ${room}에 입장했습니다.`
+    socket.to(room).emit("receive_message", { message: joinMessage, user: "관리자" })
+  })
 })
 
 server.listen(3001, () => {
-    console.log("SERVER IS RUNNING")
+  console.log("SERVER IS RUNNING")
 })
